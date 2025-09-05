0dbc8ab62d54a8ddc186e171f9a6b8b83cbc26cc
Rodrigo Teixeira de Andrade <rodrigoandrade20@yahoo.com.br>
2025-09-04 21:31:07 -0300

    refactor: adjust webpack file

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,20 +1,18 @@
 const { withModuleFederationPlugin, shareAll } = require('@angular-architects/module-federation/webpack');
 
-/**
- * Configuração do Module Federation otimizada para produção
- * com foco em evitar o erro "Shared module is not available for eager consumption"
- */
 module.exports = withModuleFederationPlugin({
+  output: {
+    publicPath: "https://taskflow-component.netlify.app/"
+  },
   name: 'taskflowComponent',
+  filename: 'remoteEntry.js',
   exposes: {
     './Module': './src/app/app.component.ts',
     './Routes': './src/app/app.routes.ts',
     './ProjectListComponent': './src/app/features/project/project-list.component.ts'
   },
-  // Configuração de compartilhamento para produção
   shared: {
     ...shareAll({ singleton: true, strictVersion: false, requiredVersion: false, eager: false }),
   },
-  // Usar um formato de biblioteca mais compatível
   library: { type: 'var', name: 'taskflowComponent' },
 });
